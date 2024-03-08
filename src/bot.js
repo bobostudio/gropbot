b3a664feeb03906e3485e5a3d5accaf4b589a0f3
bobostudio <m18576493832@163.com>
2024-03-09 00:29:14 +0800

    feat: init project

--- src/bot.js
+++ src/bot.js
@@ -4,26 +4,36 @@ import Groq from "groq-sdk";
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY || "your_groq_api_key",
 });
+
 const bot = new Bot(
-  process.env.TELEGRAM_BOT_TOKEN || "your_telegram_bot_token"
+  process.env.TELEGRAM_BOT_TOKEN || "your_telegram_bot_token",
 );
 
 async function getGroqResponse(query) {
   try {
     const completion = await groq.chat.completions.create({
-      messages: [{ role: "user", content: query }],
+      messages: [
+        {
+          role: "user",
+          content: query,
+        },
+      ],
       model: "mixtral-8x7b-32768",
     });
+
     return completion.choices[0].message.content;
-  } catch (e) {
-    console.error(e);
+  } catch (error) {
+    console.error(error);
   }
 }
-bot.on('message:text',async (ctx)=>{
-    const res = await getGroqResponse(ctx.message.text);
-    console.log('Message from:',ctx.from.username);
-    console.log('Message:',ctx.message.text)
-    ctx.reply(res);
-})
+
+bot.on("message:text", async (ctx) => {
+  const response = await getGroqResponse(ctx.message.text);
+
+  console.log('Message from: ', ctx.from.username)
+  console.log('Message: ', ctx.message.text)
+
+  ctx.reply(response);
+});
 
 bot.start();
